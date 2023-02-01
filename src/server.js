99796af125090ac7fc62d2cf43f243a0c8c8bfb9
Mark McCarthy <mark.joseph.mccarthy@gmail.com>
2023-02-01 15:25:50 +0000

    change port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const userRouter = require("./user/user.routes");
 // const initModels = require("./models/init-models");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors());
